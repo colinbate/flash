9d92217718309c3d27f57b1246e57b333217693d
Colin Bate <colin@colinbate.com>
2020-05-19 15:20:19 -0300

    Add more decks

--- src/EndOfDeck.svelte
+++ src/EndOfDeck.svelte
@@ -13,6 +13,14 @@ export let total = 0;
   justify-content: center;
   height: 80vh;
 }
+.buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 60vw;
+}
+
 .score {
   font-size: 2rem;
   margin: 1rem;
@@ -26,13 +34,23 @@ button {
   margin: 1rem;
   pointer-events: fill;
   width: 80vw;
-  max-width: 30rem;
+  max-width: 25rem;
+}
+@media (orientation: landscape) {
+  .menu-card {
+    flex-direction: row;
+  }
+  button {
+    width: 80%;
+  }
 }
 </style>
 
 <div class="menu-card">
   <div class="score">{score} / {total}</div>
-  <button type="button" on:click={() => dispatch('restart', {trim: false})}>Restart</button>
-  <button type="button" on:click={() => dispatch('restart', {trim: true})}>Restart without correct</button>
-  <button type="button" on:click={() => dispatch('reset')}>Reset deck</button>
+  <div class="buttons">
+    <button type="button" on:click={() => dispatch('restart', {trim: false})}>Restart</button>
+    <button type="button" on:click={() => dispatch('restart', {trim: true})}>Restart without correct</button>
+    <button type="button" on:click={() => dispatch('reset')}>Reset deck</button>
+  </div>
 </div>
